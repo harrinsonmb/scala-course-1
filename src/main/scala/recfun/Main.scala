2d861207bfd48ac41c540ac91eae9c3c9a6753c2
Harrinson Mosquera <harrinsonmb@hotmail.es>
2017-07-13 23:39:21 +0200

    - Improvements in exercise 3

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -2,12 +2,16 @@ package recfun
 
 object Main {
   def main(args: Array[String]) {
-    println("Pascal's Triangle")
-    for (row <- 0 to 10) {
-      for (col <- 0 to row)
-        print(pascal(col, row) + " ")
-      println()
-    }
+//    println("Pascal's Triangle")
+//    for (row <- 0 to 10) {
+//      for (col <- 0 to row)
+//        print(pascal(col, row) + " ")
+//      println()
+//    }
+    println(countChange(4,List(1,2)))
+    println(countChange(300,List(5,10,20,50,100,200,500)))
+    println(countChange(301,List(5,10,20,50,100,200,500)))
+    println(countChange(300,List(500,5,50,100,20,200,10)))
   }
 
   /**
@@ -25,20 +29,46 @@ object Main {
    */
     def balance(chars: List[Char]): Boolean = {
       //si encuentra uno abierto busque uno cerrado si encuentra otro abierto siga buscando
-      val sublist = chars.filter( c => c == '(' || c == ')');
+      val sublist = chars.filter( c => c == '(' || c == ')')
       if(sublist.head == '('){
         if(sublist.tail.head  == ')' && sublist.tail.head == sublist.tail.last){
-          return true;
+          true
         }else{
-          balance(sublist.tail);
+          balance(sublist.tail)
         }
       }else{
-        return false;
+        false
       }
     }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = 0
+    def countChange(money: Int, coins: List[Int]): Int = {
+      val filtered = coins.filter(coin => money % coin == 0).sortWith(_ > _)
+      var num = 0
+
+      if (filtered.isEmpty)
+       return 0
+
+      //necesitamos saber en cuantas partes se puede descomponer
+
+
+      if(filtered.tail.nonEmpty){
+        if(filtered.head-filtered.tail.head < money){
+          if(money % filtered.head == 0){
+            num+= money / filtered.head
+            countChange(filtered.head, filtered.tail)
+          }else{
+            countChange(filtered.head, filtered.tail)
+          }
+          println("Entra arriba")
+        }else{
+          num += filtered.tail.count( current => current%filtered.head == 0)
+          println("Entra abajo")
+        }
+      }
+
+      num
+    }
   }
